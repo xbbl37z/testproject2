93a96b121671730a474120ab872a978fd2c355c0
DELL <DELL@DESKTOP-GAVU684>
2020-06-25 21:39:46 +0530

    commit-1

---
+++ apptwo/src/main/java/simple_app2/apptwo/App.java
@@ -0,0 +1,13 @@
+package simple_app2.apptwo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
